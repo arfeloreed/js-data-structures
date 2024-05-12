f78c6bf5ff4f1da65cc148fcb5186aced599b037
reedpogi <reed@reedpogi.com>
2024-05-12 09:41:24 +0800

    modified bst

--- binary-search-tree/bst.js
+++ binary-search-tree/bst.js
@@ -61,14 +61,93 @@ class BinarySearchTree {
 
     return toReturn;
   }
+
+  // using other methods for searching
+  // BFS (breadth-first search)
+  bfs() {
+    let queue = [];
+    let toReturn = [];
+
+    if (!this.root) return toReturn;
+    queue.push(this.root);
+
+    while (queue.length !== 0) {
+      const current = queue.shift();
+
+      if (current.left) queue.push(current.left);
+      if (current.right) queue.push(current.right);
+
+      toReturn.push(current.val);
+    }
+
+    return toReturn;
+  }
+
+  // Depth-first search (DFS)
+  preOrder() {
+    let toReturn = [];
+
+    function check(node) {
+      toReturn.push(node.val);
+
+      if (node.left) check(node.left);
+      if (node.right) check(node.right);
+
+      return;
+    }
+
+    if (!this.root) return toReturn;
+    let current = this.root;
+    check(current);
+
+    return toReturn;
+  }
+
+  postOrder() {
+    let toReturn = [];
+
+    function check(node) {
+      if (node.left) check(node.left);
+      if (node.right) check(node.right);
+
+      return toReturn.push(node.val);
+    }
+
+    if (!this.root) return toReturn;
+    let current = this.root;
+    check(current);
+
+    return toReturn;
+  }
+
+  inOrder() {
+    let toReturn = [];
+
+    function check(node) {
+      if (node.left) check(node.left);
+      toReturn.push(node.val);
+      if (node.right) check(node.right);
+
+      return;
+    }
+
+    if (!this.root) return toReturn;
+    let current = this.root;
+    check(current);
+
+    return toReturn;
+  }
 }
 
 const bst = new BinarySearchTree();
 bst.insert(10);
 bst.insert(11);
-// bst.insert(10);
 bst.insert(5);
 bst.insert(7);
 bst.insert(3);
 bst.insert(13);
-console.log(bst.get(5));
+// console.log(bst.get(13));
+console.log(bst.bfs());
+console.log(bst.preOrder());
+console.log(bst.postOrder());
+console.log(bst.inOrder());
